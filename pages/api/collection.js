47f4ff645b689a5fd091fd2b4bb382a2fa933252
Doğukan Doğu <dogukandogu@piton.com.tr>
2023-04-26 09:53:32 +0300

    first commit

---
+++ pages/api/collection.js
@@ -0,0 +1,515 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    coins: [
+      {
+        id: 1,
+        countryId: 1,
+        denomination: "50 So'm",
+        description: "Central Bank of Uzbekistan",
+        year: "2018",
+        observe:
+          "https://i.ucoin.net/coin/37/789/37789867-1/uzbekistan-50-som-2018.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/37/789/37789867-2/uzbekistan-50-som-2018.jpg",
+      },
+      {
+        id: 2,
+        countryId: 1,
+        denomination: "100 So'm",
+        description: "Central Bank of Uzbekistan",
+        year: "2018",
+        observe:
+          "https://i.ucoin.net/coin/50/235/50235233-1/uzbekistan-100-som-2018.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/50/235/50235233-2/uzbekistan-100-som-2018.jpg",
+      },
+      {
+        id: 3,
+        countryId: 1,
+        denomination: "200 So'm",
+        description: "Central Bank of Uzbekistan",
+        year: "2018",
+        observe:
+          "https://i.ucoin.net/coin/40/503/40503061-1/uzbekistan-200-som-2018.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/40/503/40503061-2/uzbekistan-200-som-2018.jpg",
+      },
+      {
+        id: 4,
+        countryId: 2,
+        denomination: "10 Kopiyok",
+        description: "Oak Leaf",
+        year: 2009,
+        observe:
+          "https://i.ucoin.net/coin/45/591/45591973-1/ukraine-10-kopiyok-2009.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/45/591/45591973-2/ukraine-10-kopiyok-2009.jpg",
+      },
+      {
+        id: 5,
+        countryId: 2,
+        denomination: "10 Kopiyok",
+        description: "Oak Leaf",
+        year: 2010,
+        observe:
+          "https://i.ucoin.net/coin/45/593/45593177-1/ukraine-10-kopiyok-2010.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/45/593/45593177-2/ukraine-10-kopiyok-2010.jpg",
+      },
+      {
+        id: 6,
+        countryId: 2,
+        denomination: "10 Kopiyok",
+        description: "Oak Leaf",
+        year: 2013,
+        observe:
+          "https://i.ucoin.net/coin/45/584/45584641-1/ukraine-25-kopiyok-2013.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/45/584/45584641-2/ukraine-25-kopiyok-2013.jpg",
+      },
+      {
+        id: 7,
+        countryId: 2,
+        denomination: "10 Kopiyok",
+        description: "Oak Leaf",
+        year: 2019,
+        observe:
+          "https://i.ucoin.net/coin/41/167/41167598-1/ukraine-10-kopiyok-2019.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/41/167/41167598-2/ukraine-10-kopiyok-2019.jpg",
+      },
+      {
+        id: 8,
+        countryId: 2,
+        denomination: "50 Kopiyok",
+        description: "Oak Leaf",
+        year: 1992,
+        observe:
+          "https://i.ucoin.net/coin/43/780/43780420-1/ukraine-50-kopiyok-1992.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/43/780/43780420-2/ukraine-50-kopiyok-1992.jpg",
+      },
+      {
+        id: 9,
+        countryId: 2,
+        denomination: "50 Kopiyok",
+        description: "Oak Leaf",
+        year: 2007,
+        observe:
+          "https://i.ucoin.net/coin/22/584/22584010-1/ukraine-50-kopiyok-2007.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/22/584/22584010-2/ukraine-50-kopiyok-2007.jpg",
+      },
+      {
+        id: 10,
+        countryId: 2,
+        denomination: "50 Kopiyok",
+        description: "Oak Leaf",
+        year: 2009,
+        observe:
+          "https://i.ucoin.net/coin/1/038/1038456-1/ukraine-50-kopiyok-2009.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/1/038/1038456-2/ukraine-50-kopiyok-2009.jpg",
+      },
+      {
+        id: 11,
+        countryId: 2,
+        denomination: "50 Kopiyok",
+        description: "Oak Leaf",
+        year: 2013,
+        observe:
+          "https://i.ucoin.net/coin/38/178/38178064-1/ukraine-50-kopiyok-2013.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/38/178/38178064-2/ukraine-50-kopiyok-2013.jpg",
+      },
+      {
+        id: 12,
+        countryId: 2,
+        denomination: "50 Kopiyok",
+        description: "Oak Leaf",
+        year: 2014,
+        observe:
+          "https://i.ucoin.net/coin/39/402/39402972-1/ukraine-50-kopiyok-2014.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/39/402/39402972-2/ukraine-50-kopiyok-2014.jpg",
+      },
+      {
+        id: 13,
+        countryId: 2,
+        denomination: "50 Kopiyok",
+        description: "Oak Leaf",
+        year: 2018,
+        observe:
+          "https://i.ucoin.net/coin/48/195/48195592-1/ukraine-50-kopiyok-2018.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/48/195/48195592-2/ukraine-50-kopiyok-2018.jpg",
+      },
+      {
+        id: 14,
+        countryId: 2,
+        denomination: "1 Grivna",
+        description: "Vladimir the Great (Grand Prince of Kiev)",
+        year: 2014,
+        observe:
+          "https://i.ucoin.net/coin/33/428/33428880-2/ukraine-1-hryvnia-2014.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/33/428/33428880-1/ukraine-1-hryvnia-2014.jpg",
+      },
+      {
+        id: 15,
+        countryId: 2,
+        denomination: "1 Grivna",
+        description: "Vladimir the Great (Grand Prince of Kiev)",
+        year: 2018,
+        observe:
+          "https://i.ucoin.net/coin/18/480/18480125-2/ukraine-1-hryvnia-2018.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/18/480/18480125-1/ukraine-1-hryvnia-2018.jpg",
+      },
+      {
+        id: 16,
+        countryId: 2,
+        denomination: "1 Grivna",
+        description: "Vladimir the Great (Grand Prince of Kiev)",
+        year: 2019,
+        observe:
+          "https://i.ucoin.net/coin/41/183/41183802-2/ukraine-1-hryvnia-2019.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/41/183/41183802-1/ukraine-1-hryvnia-2019.jpg",
+      },
+      {
+        id: 17,
+        countryId: 2,
+        denomination: "1 Grivna",
+        description: "Vladimir the Great (Grand Prince of Kiev)",
+        year: 2020,
+        observe:
+          "https://i.ucoin.net/coin/36/507/36507917-2/ukraine-1-hryvnia-2020.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/36/507/36507917-1/ukraine-1-hryvnia-2020.jpg",
+      },
+      {
+        id: 18,
+        countryId: 2,
+        denomination: "2 Grivna",
+        description: "Vladimir the Great (Grand Prince of Kiev)",
+        year: 2018,
+        observe:
+          "https://i.ucoin.net/coin/36/302/36302873-2/ukraine-2-hryvni-2018.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/36/302/36302873-1/ukraine-2-hryvni-2018.jpg",
+      },
+      {
+        id: 19,
+        countryId: 2,
+        denomination: "2 Grivna",
+        description: "Vladimir the Great (Grand Prince of Kiev)",
+        year: 2019,
+        observe:
+          "https://i.ucoin.net/coin/33/082/33082368-2/ukraine-2-hryvni-2019.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/33/082/33082368-1/ukraine-2-hryvni-2019.jpg",
+      },
+      {
+        id: 20,
+        countryId: 2,
+        denomination: "2 Grivna",
+        description: "Vladimir the Great (Grand Prince of Kiev)",
+        year: 2020,
+        observe:
+          "https://i.ucoin.net/coin/36/710/36710697-2/ukraine-2-hryvni-2020.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/36/710/36710697-1/ukraine-2-hryvni-2020.jpg",
+      },
+      {
+        id: 21,
+        countryId: 2,
+        denomination: "2 Grivna",
+        description: "Vladimir the Great (Grand Prince of Kiev)",
+        year: 2021,
+        observe:
+          "https://i.ucoin.net/coin/46/572/46572954-2/ukraine-2-hryvni-2021.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/46/572/46572954-1/ukraine-2-hryvni-2021.jpg",
+      },
+      {
+        id: 22,
+        countryId: 2,
+        denomination: "5 Grivna",
+        description: "Bohdan Khmelnytsky",
+        year: 2019,
+        observe:
+          "https://i.ucoin.net/coin/33/081/33081573-2/ukraine-5-hryvnias-2019.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/33/081/33081573-1/ukraine-5-hryvnias-2019.jpg",
+      },
+      {
+        id: 23,
+        countryId: 3,
+        denomination: "1 Ley",
+        description: "REPUBLICA MOLDOVA",
+        year: 2020,
+        observe:
+          "https://i.ucoin.net/coin/52/967/52967035-1/moldova-1-leu-2020.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/52/967/52967035-2/moldova-1-leu-2020.jpg",
+      },
+      {
+        id: 24,
+        countryId: 4,
+        denomination: "5 Cent",
+        description: "Elizabeth II.",
+        year: 1993,
+        observe:
+          "https://i.ucoin.net/coin/29/554/29554385-1/australia-5-cents-1993.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/29/554/29554385-2/australia-5-cents-1993.jpg",
+      },
+      {
+        id: 25,
+        countryId: 5,
+        denomination: "1 Lev",
+        description: "St. Ivan Rilski",
+        observe:
+          "https://i.ucoin.net/coin/5/359/5359800-1/bulgaria-1-lev-2002.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/5/359/5359800-2/bulgaria-1-lev-2002.jpg",
+        year: 2002,
+      },
+      {
+        id: 26,
+        countryId: 6,
+        denomination: "1 Zloty",
+        description: "Eagle (Heraldry)",
+        year: 2014,
+        observe:
+          "https://i.ucoin.net/coin/40/949/40949468-1/poland-1-zloty-2014.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/40/949/40949468-2/poland-1-zloty-2014.jpg",
+      },
+      {
+        id: 27,
+        countryId: 6,
+        denomination: "5 Zloty",
+        description: "Eagle (Heraldry).",
+        year: 2016,
+        observe:
+          "https://i.ucoin.net/coin/49/505/49505376-1/poland-5-zlotych-2016.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/49/505/49505376-2/poland-5-zlotych-2016.jpg",
+      },
+      {
+        id: 28,
+        countryId: 7,
+        denomination: "50 Kopek",
+        description: "Saint George",
+        year: 2013,
+        observe:
+          "https://i.ucoin.net/coin/14/866/14866156-1/russia-50-kopeks-2013.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/14/866/14866156-2/russia-50-kopeks-2013.jpg",
+      },
+      {
+        id: 29,
+        countryId: 8,
+        denomination: "10 Eurocent",
+        description: "Juan Carlos I.",
+        year: 1999,
+        observe:
+          "https://i.ucoin.net/coin/55/784/55784816-1/spain-10-euro-cent-1999.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/55/784/55784816-2/spain-10-euro-cent-1999.jpg",
+      },
+      {
+        id: 30,
+        countryId: 8,
+        denomination: "1 Euro",
+        description: "Juan Carlos I.",
+        year: 2000,
+        observe: "https://i.ucoin.net/coin/0/0/91-1/spain-1-euro-2000.jpg",
+        reverse: "https://i.ucoin.net/coin/0/0/91-2/spain-1-euro-2000.jpg",
+      },
+      {
+        id: 31,
+        countryId: 8,
+        denomination: "1 Euro",
+        description: "Juan Carlos I.",
+        year: 2002,
+        observe:
+          "https://i.ucoin.net/coin/55/786/55786288-1/spain-1-euro-2002.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/55/786/55786288-2/spain-1-euro-2002.jpg",
+      },
+      {
+        id: 32,
+        countryId: 8,
+        denomination: "2 Euro",
+        description: "Juan Carlos I.",
+        year: 2001,
+        observe:
+          "https://cdn.shopify.com/s/files/1/0637/1655/7029/products/698803A_400x400.progressive.jpg?v=1672094347",
+        reverse:
+          "https://cdn.shopify.com/s/files/1/0637/1655/7029/products/698803R_400x400.progressive.jpg?v=1672094347",
+      },
+      {
+        id: 33,
+        countryId: 9,
+        denomination: "1 Euro",
+        description: "Beatrix",
+        year: 1999,
+        observe:
+          "https://www.fleur-de-coin.com/images/Thumbnails/euroimages-netherlands1euro-SIZE250x250.jpg",
+        reverse:
+          "https://www.fleur-de-coin.com/images/Thumbnails/KM200-km187_2002b-SIZE249x250.jpg",
+      },
+      {
+        id: 34,
+        countryId: 10,
+        denomination: "20 Eurocent",
+        description: "Yannis Kapodistrias",
+        year: 2002,
+        observe: "https://i.colnect.net/f/3953/880/20-Euro-Cent-back.jpg",
+        reverse: "https://i.colnect.net/f/2503/338/20-Euro-Cent.jpg",
+      },
+      {
+        id: 35,
+        countryId: 4,
+        denomination: "10 Eurocent",
+        description: "A beautiful clean typeface with punch.",
+        year: 2002,
+        observe:
+          "https://i.ucoin.net/coin/32/918/32918775-1/austria-10-euro-cent-2002.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/32/918/32918775-2/austria-10-euro-cent-2002.jpg",
+      },
+      {
+        id: 36,
+        countryId: 11,
+        denomination: "10 Eurocent",
+        description: "Planter",
+        year: 2007,
+        observe:
+          "https://i.ucoin.net/coin/0/0/318-1/france-10-euro-cent-2007.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/0/0/318-2/france-10-euro-cent-2007.jpg",
+      },
+      {
+        id: 37,
+        countryId: 12,
+        denomination: "10 Eurocent",
+        description: "The Brandenburg Gate",
+        year: 2017,
+        observe:
+          "https://onlinecoin.club/images/coins/Germany/0e586515-5fcb-4260-81ca-6e3eab7e40e5_medium.jpg",
+        reverse:
+          "https://onlinecoin.club/images/coins/Germany/8539e519-479b-4fcb-a42c-1a1025831b81_medium.jpg",
+      },
+      {
+        id: 38,
+        countryId: 12,
+        denomination: "10 Eurocent",
+        description: "The Brandenburg Gate",
+        year: 2018,
+        observe:
+          "https://i.ucoin.net/coin/21/477/21477732-1/germany-10-euro-cent-2018.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/21/477/21477732-2/germany-10-euro-cent-2018.jpg",
+      },
+      {
+        id: 39,
+        countryId: 12,
+        denomination: "10 Eurocent",
+        description: "The Brandenburg Gate",
+        year: 2020,
+        observe:
+          "https://i.ucoin.net/coin/43/672/43672551-1/germany-10-euro-cent-2020.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/43/672/43672551-2/germany-10-euro-cent-2020.jpg",
+      },
+      {
+        id: 40,
+        countryId: 12,
+        denomination: "1 Euro",
+        description: "Eagle (Heraldry)",
+        year: 2002,
+        observe:
+          "https://i.ucoin.net/coin/58/821/58821578-1/germany-1-euro-2002.jpg",
+        reverse:
+          "https://i.ucoin.net/coin/58/821/58821578-2/germany-1-euro-2002.jpg",
+      },
+      {
+        id: 41,
+        countryId: 12,
+        denomination: "2 Euro",
+        description: "Eagle (Heraldry)",
+        year: 2002,
+        reverse:
+          "https://i.ucoin.net/coin/32/919/32919058-2/germany-2-euro-2002.jpg",
+        observe:
+          "https://i.ucoin.net/coin/32/919/32919058-1/germany-2-euro-2002.jpg",
+      },
+    ],
+    countries: [
+      {
+        id: 1,
+        countryName: "Uzbekistan",
+        seoUrl: "uzbekistan",
+      },
+      {
+        id: 2,
+        countryName: "Ukrain",
+        seoUrl: "ukrain",
+      },
+      {
+        id: 3,
+        countryName: "Moldova",
+        seoUrl: "moldova",
+      },
+      {
+        id: 4,
+        countryName: "Australia",
+        seoUrl: "australia",
+      },
+      {
+        id: 5,
+        countryName: "Bulgarian",
+        seoUrl: "bulgarian",
+      },
+      {
+        id: 6,
+        countryName: "Poland",
+        seoUrl: "poland",
+      },
+      {
+        id: 7,
+        countryName: "Russia",
+        seoUrl: "russia",
+      },
+      {
+        id: 8,
+        countryName: "Spain",
+        seoUrl: "spain",
+      },
+      {
+        id: 9,
+        countryName: "Holland",
+        seoUrl: "holland",
+      },
+      {
+        id: 10,
+        countryName: "Greece",
+        seoUrl: "greece",
+      },
+      {
+        id: 11,
+        countryName: "France",
+        seoUrl: "france",
+      },
+      {
+        id: 12,
+        countryName: "Germany",
+        seoUrl: "germany",
+      },
+    ],
+  });
+}
